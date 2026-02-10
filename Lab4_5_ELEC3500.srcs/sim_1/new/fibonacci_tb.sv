d2638e8cae3aa6087fe92da9348d6a778ac59efc
Jacob Reyes <jacobreyes@ad.doe.carleton.ca>
2026-02-10 11:17:50 -0500

    Added fibonacci testbench. Will finish in lab

---
+++ Lab4_5_ELEC3500.srcs/sim_1/new/fibonacci_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/10/2026 11:05:16 AM
+// Design Name: 
+// Module Name: fibonacci_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module fibonacci_tb(
+
+    );
+endmodule
